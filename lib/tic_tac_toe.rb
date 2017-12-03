20f2f6a4c17fefe8d567a807934d3c1fc81efcc4
Nikolai Pawlowski <nikolaip@gmail.com>
2017-12-03 20:13:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,7 +64,7 @@ class TicTacToe
   end
 
   def won?
-    WIN_COMBINATIONS.detect { |c| @board[c[0]] == @board[c[1]] && @board[combo[1]] == @board[c[2]] && position_taken?(c[0]) }
+    WIN_COMBINATIONS.detect { |c| @board[c[0]] == @board[c[1]] && @board[c[1]] == @board[c[2]] && position_taken?(c[0]) }
   end
 
   def draw?
